0b9157e8337c3514995ff6f2673b54492ac1e322
John han <johndoe@example.com>
2019-06-19 16:52:26 +0800

    v1.0

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+//vue-axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
+
+//elementui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
 new Vue({
   router,
   render: h => h(App)
